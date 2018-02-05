e9528127910d61ed6659a7da344557e57fc9defa
Damian Krzeminski <pirxpilot@code42day.com>
2018-02-04 20:16:48 -0700

    stop using request
    use http/https only
    use nock module to perform all the tests

--- test/send.js
+++ test/send.js
@@ -1,224 +1,134 @@
 
-var request = require("request");
-var qs = require("querystring");
-var sinon = require("sinon");
-var url = require("url");
+var nock = require("nock");
 
-var config = require("../lib/config.js");
 var ua = require("../lib/index.js");
 
-/* global describe, it, beforeEach, afterEach */
+/* global describe, it */
 
 describe("ua", function () {
 
 	describe("#send", function () {
-		var post;
 
-		beforeEach(function () {
-			post = sinon.stub(request, "post").callsArg(2);
-		});
-
-		afterEach(function () {
-			post.restore();
-		});
-
-		it("should immidiately return with an empty queue", function () {
+		it("should immidiately return with an empty queue", function (done) {
 			var visitor = ua();
-			var fn = sinon.spy();
 
-			visitor.send(fn);
+			visitor.send(function(err, count) {
+        count.should.eql(0);
+        done(err);
+      });
 
-			post.called.should.equal(false, "no request should have been sent");
-			fn.calledOnce.should.equal(true, "callback should have been called once");
-			fn.thisValues[0].should.equal(visitor, "callback should be called in the context of the visitor instance");
-			fn.args[0].should.eql([null, 0], "no error, no requests");
 		});
 
 		it("should include data in POST body", function (done) {
-			var paramSets = [
-				{first: "123"}
-			];
-
-			var fn = sinon.spy(function () {
-				fn.calledOnce.should.equal(true, "callback should have been called once");
-				fn.thisValues[0].should.equal(visitor, "callback should be called in the context of the visitor instance");
-				fn.args[0].should.eql([null, 1], "no error, 1 requests");
-
-				post.callCount.should.equal(paramSets.length, "each param set should have been POSTed");
-
-				for (var i = 0; i < paramSets.length; i++) {
-					var params = paramSets[i];
-					var args = post.args[i];
-
-					var parsedUrl = url.parse(args[0]);
-
-					Math.random(); // I have absolutely no idea why it fails unless there was some processing to be done after url.parse…
-
-					(parsedUrl.protocol + "//" + parsedUrl.host).should.equal(config.hostname);
-					args[1].body.should.equal(qs.stringify(params));
-				}
-
-				done();
-			});
+      nock('http://www.google-analytics.com')
+        .post('/collect', 'first=124')
+        .reply(204);
 
 			var visitor = ua();
-			visitor._queue.push.apply(visitor._queue, paramSets);
-			visitor.send(fn);
+			visitor._queue.push({ first: 124 });
+			visitor.send(function(err, count) {
+        count.should.eql(1);
+        done(err);
+      });
 		});
 
 		it("should send individual requests when batchting is false", function(done) {
+      nock('http://www.google-analytics.com')
+        .post('/collect', 'first=124')
+        .reply(204)
+        .post('/collect', 'second=abc')
+        .reply(204)
+        .post('/collect', 'third=false')
+        .reply(204);
+
 			var paramSets = [
-				{first: Math.random()},
-				{second: Math.random()},
-				{third: Math.random()}
+				{ first: 124 },
+				{ second: 'abc' },
+				{ third: false }
 			];
-
-			var fn = sinon.spy(function () {
-				fn.calledOnce.should.equal(true, "callback should have been called once");
-				fn.thisValues[0].should.equal(visitor, "callback should be called in the context of the visitor instance");
-
-				fn.args[0].should.eql([null, 3], "no error, 3 requests");
-
-				done();
-			});
 
 			var visitor = ua({enableBatching:false});
 			visitor._queue.push.apply(visitor._queue, paramSets);
-			visitor.send(fn);
+			visitor.send(function(err, count) {
+        count.should.eql(3);
+        done(err);
+      });
 		});
 
 		describe("#batching is true", function() {
+
 			it("should send request to collect path when only one payload", function(done) {
+        nock('http://www.google-analytics.com')
+          .post('/collect', 'first=1234')
+          .reply(204);
+
 				var paramSets = [
-					{first: Math.random()}
+					{ first: 1234 }
 				];
-
-				var fn = sinon.spy(function () {
-					fn.args[0].should.eql([null, 1], "no error, 1 requests");
-					var args = post.args[0];
-
-					var parsedUrl = url.parse(args[0]);
-
-					parsedUrl.pathname.should.eql(config.path);
-					done();
-				});
 
 				var visitor = ua({enableBatching:true});
 				visitor._queue.push.apply(visitor._queue, paramSets);
-				visitor.send(fn);
+				visitor.send(function(err, count) {
+          count.should.eql(1);
+          done(err);
+        });
 			});
 
 			it("should send request to batch path when more than one payload sent", function(done) {
-				var paramSets = [
-					{first: Math.random()},
-					{second: Math.random()},
-					{third: Math.random()}
-				];
+        nock('http://www.google-analytics.com')
+          .post('/batch', 'first=123\nsecond=abc\nthird=false')
+          .reply(204);
 
-				var fn = sinon.spy(function () {
-					fn.args[0].should.eql([null, 1], "no error, 1 requests");
-					var args = post.args[0];
-
-					var parsedUrl = url.parse(args[0]);
-
-					parsedUrl.pathname.should.eql(config.batchPath);
-					done();
-				});
+        var paramSets = [
+          { first: 123 },
+          { second: 'abc' },
+          { third: false }
+        ];
 
 				var visitor = ua({enableBatching:true});
 				visitor._queue.push.apply(visitor._queue, paramSets);
-				visitor.send(fn);
-			});
-
-			it("should batch data in Post form", function(done) {
-				var paramSets = [
-					{first: Math.random()},
-					{second: Math.random()},
-					{third: Math.random()}
-				];
-
-				var fn = sinon.spy(function () {
-					fn.calledOnce.should.equal(true, "callback should have been called once");
-					fn.thisValues[0].should.equal(visitor, "callback should be called in the context of the visitor instance");
-
-					fn.args[0].should.eql([null, 1], "no error, 1 requests");
-					var args = post.args[0];
-
-					var params = paramSets;
-					var formParams = args[1].body.split("\n");
-					formParams.should.have.lengthOf(3);
-					formParams[0].should.equal(qs.stringify(params[0]));
-
-					done();
-				});
-
-				var visitor = ua({enableBatching:true});
-				visitor._queue.push.apply(visitor._queue, paramSets);
-				visitor.send(fn);
+				visitor.send(function(err, count) {
+          count.should.eql(1);
+          done(err);
+        });
 			});
 
 			it("should batch data based on batchSize", function(done) {
-				var paramSets = [
-					{first: Math.random()},
-					{second: Math.random()},
-					{third: Math.random()}
-				];
+        nock('http://www.google-analytics.com')
+          .post('/batch', 'first=123\nsecond=abc')
+          .reply(204)
+          .post('/collect', 'third=false')
+          .reply(204);
 
-				var fn = sinon.spy(function () {
-					fn.calledOnce.should.equal(true, "callback should have been called once");
-					fn.thisValues[0].should.equal(visitor, "callback should be called in the context of the visitor instance");
-
-					fn.args[0].should.eql([null, 2], "no error, 2 requests");
-
-					var body = post.args[0][1].body;
-
-					body.split("\n").should.have.lengthOf(2);
-
-					done();
-				});
+        var paramSets = [
+          { first: 123 },
+          { second: 'abc' },
+          { third: false }
+        ];
 
 				var visitor = ua({enableBatching:true, batchSize: 2});
 				visitor._queue.push.apply(visitor._queue, paramSets);
-				visitor.send(fn);
+				visitor.send(function(err, count) {
+          count.should.eql(2);
+          done(err);
+        });
 			});
+
 		});
 
 		it("should add custom headers to request header", function (done) {
-			var fn = sinon.spy(function () {
-				fn.calledOnce.should.equal(true, "callback should have been called once");
-				fn.thisValues[0].should.equal(visitor, "callback should be called in the context of the visitor instance");
-
-				post.calledOnce.should.equal(true, "request should have been POSTed");
-
-				var parsedUrl = url.parse(post.args[0][0]);
-				var options = post.args[0][1];
-
-				(parsedUrl.protocol + "//" + parsedUrl.host).should.equal(config.hostname);
-
-				options.should.have.keys("headers","body");
-				options.headers.should.have.key("User-Agent");
-				options.headers["User-Agent"].should.equal("Test User Agent");
-
-				done();
-			});
+      nock('http://www.google-analytics.com')
+        .matchHeader('User-Agent', 'Test User Agent')
+        .post('/collect', '')
+        .reply(204);
 
 			var visitor = ua({
 				headers: {'User-Agent': 'Test User Agent'}
 			});
 			visitor._queue.push({});
-			visitor.send(fn);
+			visitor.send(done);
 		});
 
 	});
 
 });
-
-
-
-
-
-
-
-
-
-
